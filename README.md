1acfb0721ad1329ca9ad258079927d89d1bf7f7b
te1m0z <dmitriy.balamozhnov@gmail.com>
2022-11-02 21:32:57 +0400

    refactor stylelint & files & debug .run

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## Установка ESLint в typescript-проект:
 * npm i -D eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin
+
+## Установка StyleLint в проект:
+* npm i -D stylelint stylelint-config-standard stylelint-order stylelint-order-config-standard
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+/* Styles */
+import '../styles/globals.css'
+
+function App({ Component, pageProps }: AppProps): JSX.Element {
     return <Component { ...pageProps } />
 }
+
+export default App
