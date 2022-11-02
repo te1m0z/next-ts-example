39f94b6d83bea4f188fa23c3f45335389bcb3db0
te1m0z <dmitriy.balamozhnov@gmail.com>
2022-11-02 22:22:15 +0400

    added lang attr to Html

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,19 @@ import type { AppProps } from 'next/app'
 
 /* Styles */
 import '../styles/globals.css'
+import Head              from 'next/head'
 
-function App({ Component, pageProps }: AppProps): JSX.Element {
-    return <Component { ...pageProps } />
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+
+    return (
+        <>
+            <Head>
+                <title>Next.ts Example App</title>
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+            <Component { ...pageProps } />
+        </>
+    )
 }
 
 export default App
